74d2b910c7788629d5afd5cabd1757c78bec2b44
Jeffrey Linwood <jlinwood@gmail.com>
2012-03-10 09:13:45 -0600

    Initial Commit

---
+++ DrupalMapApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DrupalMapApp
+//
+//  Created by Jeffrey Linwood on 3/10/12.
+//  Copyright (c) 2012 Jeff Linwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DrupalMapApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DrupalMapApp
+//
+//  Created by Jeffrey Linwood on 3/10/12.
+//  Copyright (c) 2012 Jeff Linwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
